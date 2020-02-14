31133c73713f2df02bd5304ac46fadeeb558e2da
NMPavan <manikyapavan123@gmail.com>
2020-02-14 13:47:00 +0530

    book layout and recyclerview layout design has been completed

---
+++ app/src/main/java/androidapp/yashthaluri/com/yeoman/DetailWorkersShowActivity.java
@@ -0,0 +1,14 @@
+package androidapp.yashthaluri.com.yeoman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailWorkersShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_workers_show);
+    }
+}
