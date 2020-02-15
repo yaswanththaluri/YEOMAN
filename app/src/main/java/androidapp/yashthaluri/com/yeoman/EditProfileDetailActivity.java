dd47e8caaacd403e8f739fd57870e03124f94d25
T B Yaswanth Kumar <thaluri.official@gmail.com>
2020-02-15 08:26:59 +0530

    Profile Edit UI Redesigned.

--- app/src/main/java/androidapp/yashthaluri/com/yeoman/EditProfileDetailActivity.java
+++ app/src/main/java/androidapp/yashthaluri/com/yeoman/EditProfileDetailActivity.java
@@ -1,5 +1,6 @@
 package androidapp.yashthaluri.com.yeoman;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -16,8 +17,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -26,29 +30,23 @@ import androidapp.yashthaluri.com.yeoman.databinding.ActivityEditProfileDetailBi
 
 public class EditProfileDetailActivity extends AppCompatActivity {
     ActivityEditProfileDetailBinding binding;
-    private Calendar calendar;
-    private SimpleDateFormat simpleDateFormat;
-    private int year, month, day;
-    private LinearLayout info;
     private FirebaseAuth auth;
     private FirebaseUser user;
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
     private ProgressDialog dialog;
+    private boolean editFabFlag = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_edit_profile_detail);
         
-        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        binding.fabEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                binding.profileDetailsShow.setVisibility(View.INVISIBLE);
-                binding.visibleEditprofile.setVisibility(View.VISIBLE);
+
             }
         });
-        simpleDateFormat = new SimpleDateFormat("dd:MMM:YYYY");
-
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
@@ -58,26 +56,17 @@ public class EditProfileDetailActivity extends AppCompatActivity {
         dialog = new ProgressDialog(this);
         dialog.setCanceledOnTouchOutside(false);
 
-        binding.dob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectBirthDate();
-            }
-        });
 
-        binding.submitBTN.setEnabled(false);
+        binding.submitBTN1.setEnabled(false);
         addChangeListener();
-        binding.submitBTN.setOnClickListener(new View.OnClickListener() {
+        binding.submitBTN1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (binding.submitBTN.isEnabled()) {
+                if (binding.submitBTN1.isEnabled()) {
                     if (TextUtils.isEmpty(binding.fullName.getText().toString())) {
                         binding.fullName.setError("Please enter your First Name");
                         binding.fullName.requestFocus();
-                    } else if (TextUtils.isEmpty(binding.dob.getText().toString())) {
-                        binding.dob.setError("Please select your Date of Birth");
-                        binding.dob.requestFocus();
-                    } else if (TextUtils.isEmpty(binding.pincode.getText().toString())) {
+                    }  else if (TextUtils.isEmpty(binding.pincode.getText().toString())) {
                         binding.pincode.setError("Please enter your PIN CODE");
                         binding.pincode.requestFocus();
                     } else if (TextUtils.isEmpty(binding.address.getText().toString())) {
@@ -113,6 +102,25 @@ public class EditProfileDetailActivity extends AppCompatActivity {
 
         });
 
+        binding.fabEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editFabFlag)
+                {
+                    binding.fabEdit.setImageResource(R.drawable.edit);
+                    stopFocusForEditFields();
+                    binding.submitBTN1.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    binding.fabEdit.setImageResource(R.drawable.canceledit);
+                    showFocusForEditFields();
+                    binding.submitBTN1.setVisibility(View.VISIBLE);
+                }
+                editFabFlag= !editFabFlag;
+            }
+        });
+
     }
 
 
@@ -121,7 +129,6 @@ public class EditProfileDetailActivity extends AppCompatActivity {
         binding.AadharNumber.addTextChangedListener(watcher);
         binding.pincode.addTextChangedListener(watcher);
         binding.address.addTextChangedListener(watcher);
-        binding.dob.addTextChangedListener(watcher);
         binding.state.addTextChangedListener(watcher);
 
     }
@@ -139,69 +146,17 @@ public class EditProfileDetailActivity extends AppCompatActivity {
 
         @Override
         public void afterTextChanged(Editable editable) {
-            if (binding.fullName.getText().toString().length() == 0 ||binding.state.getText().toString().length() ==0 || binding.AadharNumber.getText().toString().length() == 0 || binding.pincode.getText().toString().length() == 0 || binding.address.getText().toString().length() == 0 || binding.dob.getText().toString().length() == 0) {
-                binding.submitBTN.setEnabled(false);
+            if (binding.fullName.getText().toString().length() == 0 ||binding.state.getText().toString().length() ==0 || binding.AadharNumber.getText().toString().length() == 0 || binding.pincode.getText().toString().length() == 0 || binding.address.getText().toString().length() == 0) {
+                binding.submitBTN1.setEnabled(false);
 
             } else {
 
-                binding.submitBTN.setBackgroundColor(getResources().getColor(R.color.blue_dark));
-                binding.submitBTN.setEnabled(true);
+                binding.submitBTN1.setBackgroundColor(getResources().getColor(R.color.blue_dark));
+                binding.submitBTN1.setEnabled(true);
             }
 
         }
     };
-    private void selectBirthDate() {
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int y, int m, int d) {
-
-                switch (m + 1) {
-                    case 1:
-                        binding.dob.setText(d + " Jan " + y);
-                        break;
-                    case 2:
-                        binding.dob.setText(d + " Feb " + y);
-                        break;
-                    case 3:
-                        binding.dob.setText(d + " March " + y);
-                        break;
-                    case 4:
-                        binding.dob.setText(d + " April " + y);
-                        break;
-                    case 5:
-                        binding.dob.setText(d + " May " + y);
-                        break;
-                    case 6:
-                        binding.dob.setText(d + " June " + y);
-                        break;
-                    case 7:
-                        binding.dob.setText(d + " July " + y);
-                        break;
-                    case 8:
-                        binding.dob.setText(d + " Aug " + y);
-                        break;
-                    case 9:
-                        binding.dob.setText(d + " Sep " + y);
-                        break;
-                    case 10:
-                        binding.dob.setText(d + " Oct " + y);
-                        break;
-                    case 11:
-                        binding.dob.setText(d + " Nov " + y);
-                        break;
-                    case 12:
-                        binding.dob.setText(d + " Dec " + y);
-                        break;
-                }
-            }
-        }, year, month, day);
-        dialog.show();
-    }
-
 
     public void uploadProfileDetails()
     {
@@ -210,7 +165,6 @@ public class EditProfileDetailActivity extends AppCompatActivity {
         String fullName = binding.fullName.getText().toString();
         String aadhar = binding.AadharNumber.getText().toString();
         String gender = binding.gender.getSelectedItem().toString();
-        String dob = binding.dob.getText().toString();
         String pinCode = binding.pincode.getText().toString();
         String address = binding.address.getText().toString();
         String city = binding.city.getText().toString();
@@ -231,5 +185,50 @@ public class EditProfileDetailActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        stopFocusForEditFields();
+        prePopulateData();
+    }
 
+    public void prePopulateData()
+    {
+        databaseReference.child("users").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ProfileHelper helper = dataSnapshot.getValue(ProfileHelper.class);
+                binding.fullName.setText(helper.getUserName());
+                binding.pincode.setText(helper.getZipCode());
+                binding.address.setText(helper.getAddress());
+                binding.city.setText(helper.getVillage());
+                binding.state.setText(helper.getState());
+                binding.AadharNumber.setText(helper.getAadharNo());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void stopFocusForEditFields()
+    {
+        binding.fullName.setFocusable(false);
+        binding.gender.setFocusable(false);
+        binding.pincode.setFocusable(false);
+        binding.address.setFocusable(false);
+        binding.city.setFocusable(false);
+        binding.state.setFocusable(false);
+        binding.AadharNumber.setFocusable(false);
+    }
+
+    public void showFocusForEditFields()
+    {
+        binding.pincode.setFocusableInTouchMode(true);
+        binding.address.setFocusableInTouchMode(true);
+        binding.city.setFocusableInTouchMode(true);
+        binding.state.setFocusableInTouchMode(true);
+    }
 }
